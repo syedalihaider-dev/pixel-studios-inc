4fb1e510128129c11ca8ffb5ef62ff71e663617f
syedalihaider-dev <aleehaiderbalti@gmail.com>
2026-08-29 01:50:45 +0500

    Case Study Pages Indexing

--- src/app/case-study/[slug]/page.js
+++ src/app/case-study/[slug]/page.js
@@ -23,7 +23,7 @@ export async function generateMetadata({ params }) {
   return {
     title: caseStudy.seo.title,
     description: caseStudy.seo.description,
-    robots: { index: false, follow: false },
+    robots: { index: true, follow: true },
   };
 }
 
